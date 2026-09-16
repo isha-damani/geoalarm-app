987330d3c2664173ddfaa1c5110f5d3a0f71170d
Isha Damani <ishadamani25@gmail.com>
2026-09-16 17:40:26 +0530

    Fetch and display user's saved locations on Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,31 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 function Dashboard() {
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        async function fetchLocations(){
+            try{
+                const token = localStorage.getItem('token');
+                const response = await axios.get("http://localhost:3000/api/locations", {headers : {Authorization : `Bearer ${token}`}});
+                // console.log(response.data);
+                setLocations(response.data); 
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fetchLocations();
+    }, []);
+
     return (
         <div>
             <h2>Dashboard</h2>
+            <ul>
+                {locations.map((location) => (
+                    <li key={location._id}>{location.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
