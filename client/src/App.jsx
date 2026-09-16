0961c9bab867a07ef871534f2b3066566267e16e
Isha Damani <ishadamani25@gmail.com>
2026-09-16 15:49:42 +0530

    Add ProtectedRoute to guard the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import './App.css';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
 
   return (
     <Routes>
-      <Route path = '/' element={<Dashboard />}></Route>
+      <Route path = '/' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}></Route>
       <Route path = '/signup' element={<Signup />}></Route>
       <Route path = '/login' element={<Login />}></Route>
     </Routes>
